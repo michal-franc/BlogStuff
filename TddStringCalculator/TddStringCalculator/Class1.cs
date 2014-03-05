0ad433b022b131f056c67336881f15a71b314fb4
michal-franc <lam.michal.franc@gmail.com>
2014-03-05 21:52:14 +0000

    First Requirement - code inside the test method

--- TddStringCalculator/TddStringCalculator/Class1.cs
+++ TddStringCalculator/TddStringCalculator/Class1.cs
@@ -1,10 +1,22 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace TddStringCalculator
 {
     [TestFixture]
     public class Class1 
     {
+        [Test]
+        public void ICanParseString_AndSumUpValues()
+        {
+            var input = "1,2,3,4,5";
+            var expectedSum = 15;
 
+            var values = input.Split(',');
+
+            var actualSum = values.Select(x => int.Parse(x)).Sum();
+
+            Assert.That(actualSum, Is.EqualTo(expectedSum));
+        }
     }
 }
