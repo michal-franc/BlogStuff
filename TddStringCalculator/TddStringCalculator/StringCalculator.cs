a971acec8552436379c6ec3745bb97fc2f1dbc76
michal-franc <lam.michal.franc@gmail.com>
2014-03-26 20:32:00 +0000

    Solution to parameterized delimeter

--- TddStringCalculator/TddStringCalculator/StringCalculator.cs
+++ TddStringCalculator/TddStringCalculator/StringCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TddStringCalculator
@@ -9,9 +10,24 @@ namespace TddStringCalculator
         {
             if (string.IsNullOrWhiteSpace(input)) return 0;
 
+            var delimeterIndex = input.IndexOf("//");
+
+            var delimeters = new List<char>();
+
+            if (delimeterIndex >= 0)
+            {
+                delimeters.Add(input[delimeterIndex + 2]);
+                input = input.Substring(4);
+            }
+            else
+            {
+                delimeters.Add(',');
+                delimeters.Add('\n');
+            }
+
             try
             {
-                var values = input.Split(',','\n');
+                var values = input.Split(delimeters.ToArray());
                 return values.Select(x => int.Parse(x)).Sum();
             }
             catch (FormatException ex)
